9c69da325c5ec1d1c20999bac4235d2806829526
prmoreira23 <prm.gredes@gmail.com>
2018-07-02 16:52:55 -0400

    solved lab

--- src/index.js
+++ src/index.js
@@ -4,4 +4,70 @@ document.addEventListener('DOMContentLoaded', () => {
   const listDiv = document.getElementById("app-content");
 
   const app = new TaskLister();
+
+  const listForm = document.getElementById("create-list-form");
+
+  const appContent = document.getElementById('app-content');
+
+
+  appContent.addEventListener("click", function(event){
+
+    if(event.target.classList.contains("delete-list")){
+      let id = app.lists.findIndex(list => list.title === event.target.dataset.title);
+      app.lists.splice(id, 1);
+      event.target.parentElement.parentElement.remove();
+      appContent.innerHTML = app.render();
+    } else if(event.target.classList.contains("delete-task")){
+      let list = app.getListByTitle(event.target.dataset.listTitle);
+      let id = list.tasks.findIndex(task => task.description === event.target.dataset.title);
+      list.tasks.splice(id, 1);
+      event.target.parentElement.remove();
+      // debugger
+    }
+  });
+
+  function submitFunction(event) {
+    event.preventDefault();
+    let description = document.getElementById("new-task-description").value;
+    let priority = document.getElementById('new-task-priority').value;
+
+    let list = app.getListByTitle(document.getElementById('parent-list').value);
+    let task = new Task(list, description, priority);
+
+    appContent.innerHTML = app.render();
+    let taskForm = document.getElementById('create-task-form');
+
+    taskForm.onsubmit = submitFunction;
+
+  }
+
+  listForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let title = document.getElementById("new-list-title").value;
+
+    // let list = new List(title);
+    // app.lists.push(list);
+    let list = app.getListByTitle(title);
+    if (list) {
+      alert('List titles must be unique!!!1one1');
+      return;
+    }
+
+     list = new List(title);
+    app.lists.push(list);
+    list.id = app.lists.length - 1;
+
+    appContent.innerHTML = app.render();
+    let taskForm = document.getElementById('create-task-form');
+
+
+    taskForm.onsubmit = submitFunction;
+
+    listForm.reset()
+
+  });
+
+
+
 });
